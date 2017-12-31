4155d12c7205fb70ab8bf0d88ab736cd4654dce8
Onur GOKER <you@example.com>
2017-12-31 17:02:29 +0300

    Read file update.

--- read.py
+++ read.py
@@ -6,9 +6,13 @@ import os
 import string
 import re
 import node
+import nltk
+import math, sys
+from nltk import word_tokenize
+from nltk.corpus import stopwords
 
 path = "./attachments"
-emailPath = "./mails/phishing"
+emailPath = "mails/phishing/"
 
 
 """------------------------"""
@@ -16,7 +20,7 @@ emailPath = "./mails/phishing"
 """------------------------"""
 
 class Node(object):
-
+    
     def __init__(self, data=None, next_node=None):
         self.data = data
         self.next_node = next_node
@@ -135,7 +139,7 @@ class LinkedList(object):
 
 def mail_exists (f):
     """Checks whether eml file exists or not."""
-    return os.path.exists(os.path.join(emailPath, f))
+    return os.path.exists(os.path.join("./", f))
 
 def file_exists (f):
     """Checks whether extracted file was extracted before."""
@@ -262,45 +266,28 @@ def disgra (s):
 
 if __name__ == '__main__':
     llist = LinkedList()
-    stopWords = ["a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", 
-    "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone",
-    "anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", 
-    "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", 
-    "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", 
-    "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", 
-    "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", 
-    "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", 
-    "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", 
-    "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", 
-    "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", 
-    "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", 
-    "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", 
-    "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", 
-    "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", 
-    "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", 
-    "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", 
-    "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", 
-    "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", 
-    "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", 
-    "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"]
+
+    stopWords = set(stopwords.words('english'))
 
     #return and count all words
-    for i in range(1,150):
+    for i in range(1,301):
 
         fileName = emailPath + str(i) + ".eml" 
-        
+
         if(mail_exists(fileName)):
-            fileOpen = open(fileName, "b")
+            fileOpen = open(fileName, "rb")
             fileRead = extract(fileOpen, fileOpen.name)
-            wordList = string.split(fileRead, " ")
+
+            #tokenize
+            wordList = word_tokenize(fileRead)
 
             for word in wordList:
-
-                word = re.sub('[^A-Za-z0-9]+', '', word)
-                
                 if word not in stopWords:
+                    
+
                     llist.insert(word)
+                    print(word)
+                    sys.exit(0)
 
             fileOpen.close()
-
     llist.printList()
