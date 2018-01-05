2aa7e799b916cd84c3638d5b02b5288070203f8b
Onur GÖKER <onurgoker89@gmail.com>
2018-01-06 00:27:33 +0300

    Word count method is added.

--- custom_methods.py
+++ custom_methods.py
@@ -14,3 +14,13 @@ def isLineEmpty(line):
 def mail_exists (f):
     """Checks whether eml file exists or not."""
     return os.path.exists(os.path.join("./", f))
+
+def word_count(string):
+    my_string = string.lower().split()
+    my_dict = {}
+    for item in my_string:
+        if item in my_dict:
+            my_dict[item] += 1
+        else:
+            my_dict[item] = 1
+    print(my_dict)
