a29aafb1107d2a6e3c72174720644a7502209d61
Onur GOKER <you@example.com>
2018-01-07 16:25:59 +0300

    Parse is modified. Ham email types are changed.

--- parse.py
+++ parse.py
@@ -35,6 +35,12 @@ if sys.argv[1] != "-input":
     sys.exit()
 
 if __name__ == '__main__':
+    
+    #truncate the output file first
+    f = open('data/output/output.txt', 'r+')
+    f.truncate()
+    f.close()
+
     #return and count all words
     for i in range(1,prm.mailCount+1):
         inputFileName = inputPath + str(i) + ".eml" 
@@ -47,13 +53,10 @@ if __name__ == '__main__':
             wordList = word_tokenize(wordList) #tokenize
             readFile.close() 
 
-
+        #print the words
         if(custom_methods.mail_exists(outputFileName)):
-            writeFile       = open(outputFileName, 'w+')
-
-            for word in wordList:
-                if word not in stopWords and not word.isdigit() and word is not custom_methods.isLineEmpty(word) and word not in prm.numbers and word is not None:
+            with open(outputFileName, 'a') as the_file:
+                for word in wordList:
                     word = re.sub('[^A-Za-z0-9]+', '', word)
-                    writeFile.writelines("fdsf\n")
-
-            writeFile.close()
+                    if word and word not in stopWords and not word.isdigit() and word is not custom_methods.isLineEmpty(word) and word not in prm.numbers and word is not None and word.strip() != " " and word.strip() != "\n":
+                        the_file.write(word + '\n')
--- prm.py
+++ prm.py
@@ -2,4 +2,4 @@ import sys
 
 language = 'english'
 numbers = ["0","1","2","3","4","5","6","7","8","9"]
-mailCount = 5
+mailCount = 300
