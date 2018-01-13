4a89b03eff75a9f6a033a2cba1956c8161b8b1fa
Onur GÖKER <onurgoker89@gmail.com>
2018-01-13 22:41:33 +0300

    Output generate is modified for wordweight

--- wordweight.py
+++ wordweight.py
@@ -3,6 +3,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from textblob import TextBlob as tb
 import math, sys, prm, os, custom_methods, argparse, pandas as pd
 
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
 def tf(word, blob):
     return float(blob.words.count(word)) / int(len(blob.words))
 
@@ -14,6 +17,40 @@ def idf(word, bloblist):
 
 def tfidf(word, blob, bloblist):
     return tf(word, blob) * idf(word, bloblist)
+
+def generate_output_file(outputPath, mailType):
+    outputPath = outputPath.replace('/dict.txt','/' + mailType + '/dict.txt')
+    writeFile = open(outputPath, "w+")
+    
+    for i, blob in enumerate(bloblist):
+        res = "Document {}".format(i + 1)
+        writeFile.write(res + "\n")
+        scores = {word: tfidf(word, blob, bloblist) for word in blob.words}
+        sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+        for word, score in sorted_words:
+            res = word.decode('utf-8') + " " + str(score)
+
+            if len(word) > 1:
+                writeFile.write(res + "\n")
+    
+    writeFile.close()
+
+def order_output_keyword_list_output(outputPath, mailType):
+    outputPath = outputPath.replace('/dict.txt','/' + mailType + '/dict.txt')
+
+    with open(outputPath, 'w+') as f:
+        words = f.readlines()
+
+        #Strip the words of the newline characters (you may or may not want to do this):
+        words = [word.strip() for word in words]
+
+        #sort the list:
+        sortedLines = words.sort()
+        print(sortedLines)
+        sys.exit()
+        f.truncate()
+        f.write(sortedLines)
+        f.close()
 
 #Define Paths
 parser = argparse.ArgumentParser()
@@ -40,6 +77,7 @@ if __name__ == '__main__':
 
     arrList = []
 
+    #ham output
     for i in range(1,mailCount+1):
         outputFileName = hamInputPath + str(i) + ".eml"
 
@@ -54,18 +92,10 @@ if __name__ == '__main__':
     for i in arrList:
         bloblist.append(tb(i))
 
+    #create unique keyword list and tfid in data/output/{parameter}/output.txt
+    generate_output_file(outputPath, 'ham')
+    generate_output_file(outputPath, 'phishing')
 
-    #write results to output file
-    writeFile = open(outputPath, "w+")
-    
-    for i, blob in enumerate(bloblist):
-        res = "Document {}".format(i + 1)
-        writeFile.write(res + "\n")
-        scores = {word: tfidf(word, blob, bloblist) for word in blob.words}
-        sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
-        for word, score in sorted_words:
-            res = word + " " + str(score)
-            writeFile.write(res + "\n")
-    
-    writeFile.close()
-    
+    #sort ham and phishing files alphabetically
+    order_output_keyword_list_output(outputPath, 'ham')
+    order_output_keyword_list_output(outputPath, 'phishing')
