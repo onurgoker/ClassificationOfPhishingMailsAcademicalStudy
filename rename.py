0f7becae869f2657266be3fc8e1369062f02b3c3
Onur GOKER <onurgoker89@gmail.com>
2018-02-18 18:58:23 +0300

    Renaming is modified.

--- rename.py
+++ rename.py
@@ -1,26 +1,18 @@
-import os, sys
-
-""" 
-Renames the filenames within the same directory to be Unix friendly
-(1) Changes spaces to hyphens
-(2) Makes lowercase (not a Unix requirement, just looks better ;)
-
-Usage:
-python rename.py
-"""
+import os, sys, time
+import rename
+from shutil import copyfile
 
 def rename_mails(mailType):
     inputPath =  os.getcwd() + "/preprocessed_input_data/" + mailType + "/"
     outputPath = os.getcwd() + "/data/input/" + mailType + "/"
 
     filenames = os.listdir(inputPath)
-
     i = 1
-    for filename in filenames:
-        newName = outputPath + str(i) + ".eml"
-        os.rename(outputPath + filename, newName)
-        i = i +1
 
+    for filename in filenames:
+        newName = outputPath + str(i) + ".txt"
+        copyfile(inputPath + filename, newName)
+        i = i +1
 
 """------------------------"""
 #Program Execution
@@ -30,7 +22,7 @@ if __name__ == '__main__':
     print("Renaming ham mails...")
 
     mailType = "ham"
-    rename_mails(mailType)
+    rename.rename_mails(mailType)
 
     print("Ham mails are renamed!")
 
@@ -39,6 +31,6 @@ if __name__ == '__main__':
     print("Renaming spam mails")
 
     mailType = "phishing"
-    rename_mails(mailType)
+    rename.rename_mails(mailType)
 
     print("Phishing mails are renamed!")
